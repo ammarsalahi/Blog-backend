7f2530dc065d48368e31432290373baa5806fd6a
ammarsalahi <ammarsaalahi@gmail.com>
2024-09-24 19:18:15 +0330

    add jwt

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,5 +1,9 @@
 from rest_framework import routers
 from .views import UserViewset
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router=routers.DefaultRouter()
 
@@ -7,3 +11,10 @@ router.register('users',UserViewset,basename="users")
 
 
 urlpatterns = router.urls()
+
+urlpatterns+=[
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+]
--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'corsheaders',
-
+    'rest_framework_simplejwt',
     #apps
     'app'
 ]
@@ -137,3 +137,9 @@ MEDIA_ROOT=BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
