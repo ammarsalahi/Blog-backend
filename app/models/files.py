9433052a93de1bc2d0f925a98737eeb61d79976f
ammarsalahi <ammarsaalahi@gmail.com>
2024-10-11 20:20:55 +0330

    remove file type

--- app/models/files.py
+++ app/models/files.py
@@ -2,22 +2,17 @@ from utils.general_model import GeneralModel
 from django.db import models
 import os
 
-# docs=['docx','pdf','txt','html']
-# sounds=['mp3','wav']
-# movies=['mp4','mkv','mpeg']
-# zips=['zip','rar']
-# pictures=['png','gif','jpg','jpeg','bmp']
+docs=['docx','pdf','txt','html']
+sounds=['mp3','wav']
+movies=['mp4','mkv','mpeg']
+zips=['zip','rar']
+pictures=['png','gif','jpg','jpeg','bmp']
 
 class FileBlog(GeneralModel):
     file=models.FileField(upload_to="news/files/")
     tag=models.CharField(max_length=100,null=True,blank=True)
     states= models.CharField(default="",max_length=100)
-    filetype=models.CharField(null=True,blank=True,max_length=100)
-
-    def save(self, *args, **kwargs):
-        if self.filetype is None:
-            self.filetype="Files"     
-        super(FileBlog, self).save(*args, **kwargs)
+    
     @property
     def name(self):
         return os.path.basename(self.file.name)
