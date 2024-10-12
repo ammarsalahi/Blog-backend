fca6245d0b4c6ec0d2d970fb553f1fa0e88a59cd
ammarsalahi <ammarsaalahi@gmail.com>
2024-10-12 09:55:50 +0330

    chnage settings for database

--- backend/settings.py
+++ backend/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECURE")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -88,27 +88,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-
-#     # 'default': dj_database_url.config(default=os.getenv("DATABSE_URL"))
 #     'default': {
-
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mydb',
-#         'USER': os.getenv('db_user'),
-#         'PASSWORD': os.getenv('db_pass'),
-#         'HOST': os.getenv('db_host'),
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-
 # }
+
+DATABASES = {
+
+    # 'default': dj_database_url.config(default=os.getenv("DATABSE_URL"))
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv("DATABASE_HOST"),
+        'PORT': os.getenv("DATABASE_PORT"),
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -147,10 +147,10 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 STATIC_URL = "static/"
-# STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-     BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#      BASE_DIR / "static",
+# ]
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 
